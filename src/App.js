570a386db99da670a454932fe9a6f52c65892466
Rohit Parihar <63337003+Rohit2024-parihar@users.noreply.github.com>
2023-12-05 22:38:54 +0530

    added babel plugins

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='heading'>Hello, aa React with Webpack with nikhil!</h1>
+    </div>
+  );
+};
+
+export default App;
